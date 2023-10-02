7eddb7f7b5146a489f381a588d2bd7169c54209a
dmswn1004 <dmswnehf@naver.com>
2023-10-02 16:20:08 +0900

    [Bronze V] Title: A+B - 7, Time: 128 ms, Memory: 9524 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,6 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+for (let i = 1; i <= input[0]; i++) {
+    let [a,  b] = input[i].split(' ').map(a=> +a);
+    console.log(`Case #${i}: ${a + b}`);
+}
