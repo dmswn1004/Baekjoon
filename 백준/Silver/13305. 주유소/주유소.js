72bfc2b894cc46f45bf7a2a167e83aa4c187b9a1
dmswn1004 <dmswnehf@naver.com>
2024-04-13 01:33:07 +0900

    [Silver III] Title: 주유소, Time: 220 ms, Memory: 20444 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,14 +3,12 @@ input.shift();
 let [km, cost] = input.map((v) => v.split(" ").map(Number));
 cost.pop();
 let answer = 0;
-let min = Math.min(...cost);
+let min = cost[0];
 
 for (let i = 0; i < km.length; i++) {
-  if (cost[i] > min) answer += cost[i] * km[i];
-  else {
-    let sumKm = km.slice(i, km.length).reduce((a, b) => a + b, 0);
-    answer += min * sumKm;
-    break;
-  }
+  if (cost[i] < min) {
+    min = cost[i];
+    answer += min * km[i];
+  } else answer += min * km[i];
 }
 console.log(answer);
