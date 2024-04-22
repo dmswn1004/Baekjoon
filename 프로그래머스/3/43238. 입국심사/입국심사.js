70065e2e5e5e79653037c342d5a079741ea40d8b
dmswn1004 <dmswnehf@naver.com>
2024-04-22 21:42:57 +0900

    [level 3] Title: 입국심사, Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -0,0 +1,23 @@
+function solution(n, times) {
+    let answer = 0;
+    let start = 0;
+    let end = Math.max(...times) * n;
+    
+    times.sort((a, b) => a - b);
+    
+    while(start <= end) {
+        let mid = parseInt((start + end) / 2);
+        let total = 0;
+        let remain = Array.from({length: times.length}, v => 0);
+        
+        for(let i = 0; i < times.length; i++) {
+            total += parseInt(mid / times[i]);
+        }
+        
+        if(total >= n) {
+            answer = mid;
+            end = mid - 1;
+        } else start = mid + 1;
+    }
+    return answer;
+}
