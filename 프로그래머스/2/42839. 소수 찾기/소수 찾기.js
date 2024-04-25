da081b0f84bf9e18516519d21009d40db85687ab
dmswn1004 <dmswnehf@naver.com>
2024-04-26 02:46:13 +0900

    [level 2] Title: 소수 찾기, Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.js
@@ -0,0 +1,40 @@
+let answer = [];
+
+function solution(numbers) {
+    let num = numbers.split("").map(Number);
+    let visit = new Array(num.length).fill(false);
+
+    dfs([], 0, num, visit);
+    return answer.length;
+}
+
+
+function dfs(arr, depth, num, visit) {    
+    if(depth > num.length) return;
+    
+    let x = Number(arr.join(""));
+    
+    if (isPrime(x) === true) {
+        if(answer.indexOf(x) === -1) answer.push(x);
+    }
+    
+    for(let i = 0; i < num.length; i++) {
+        if(visit[i] === true) continue;
+        
+        arr.push(num[i]);
+        visit[i] = true;
+        dfs(arr, depth + 1, num, visit);
+        arr.pop();
+        visit[i] = false;
+    }
+}
+
+// 소수가 맞는지
+function isPrime(n) {
+    if(n < 2) return false;
+    // if(n === 2) return true;
+    for(let i = 2; i <= Math.sqrt(n); i++) {
+        if(parseInt(n) % i === 0) return false;
+    }
+    return true;
+}
